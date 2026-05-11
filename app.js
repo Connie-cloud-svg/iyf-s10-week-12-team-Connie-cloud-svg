faaec6b3b84c3316d4ddedfdbd9731869434a3f0
hunted104 <mutugian25@gmail.com>
2026-05-11 11:45:53 -0700

    Enhance app.js with root route and body parser limits
    Added a root route to respond with a success message and updated body parser limits.

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ const errorHandler = require("./middleware/errorMiddleware");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
 // Routes
-app.use("/api/auth", authRoutes);
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
-// Error middleware (MUST be last)
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "API is running successfully"
+    });
+});
+
+app.use("/api/auth", authRoutes);
+app.use((req, res, next) => {
+    const error = new Error(`Route not found: ${req.originalUrl}`);
+    error.statusCode = 404;
+    next(error);
+});
 app.use(errorHandler);
 
 module.exports = app;
